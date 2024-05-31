29167fc0d0b2dd78dcfe30f9c99c25606d03306f
doyaGu <doyaGu@users.noreply.github.com>
2024-05-31 09:07:12 -1000

    Add source files

---
+++ cmake/BMLUtils.cmake
@@ -0,0 +1,12 @@
+# - Some useful macros and functions for creating mod
+
+macro(add_bml_mod NAME)
+    add_library(${NAME} SHARED)
+    target_sources(${NAME} PRIVATE ${ARGN})
+    target_link_libraries(${NAME} PRIVATE BML CK2 VxMath)
+    set_target_properties(${NAME} PROPERTIES SUFFIX ".bmodp")
+endmacro()
+
+macro(install_bml_mod NAME)
+    install(TARGETS ${NAME} RUNTIME DESTINATION Mods)
+endmacro()
